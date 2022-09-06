7b6b2f7f07dc297ff79c95b07d65e8d893f51e83
ajithittan <ajithittan@gmail.com>
2022-09-05 21:01:04 -0400

    ok
    ok

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://192.168.1.209:5100/api/:path*' // Proxy to Backend
+        destination: 'http://localhost:5100/api/:path*' // Proxy to Backend
       },
       {
         source: '/newsfeed/:path*',
-        destination: 'http://192.168.1.209:5100/newsfeed/:path*' // Proxy to Backend
+        destination: 'http://localhost:5100/newsfeed/:path*' // Proxy to Backend
       },
     ]
   }
--- pages/Dashboard/ChartsForDashBoard.js
+++ pages/Dashboard/ChartsForDashBoard.js
@@ -6,6 +6,8 @@ import MainGrid from '../Containers'
 const ChartsForDashBoard = (props) => {
     const stockList = useAppContext()
 
+    console.log("whats this? ",stockList)
+
     const getChartItems = () => {
         if(stockList){
             return([<Charting stocks={stockList} size={7} indx={1} duration={12} name="" />])
