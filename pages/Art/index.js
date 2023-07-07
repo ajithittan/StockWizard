869863bcd063c0cbce0389f47b64b33e8cd173d7
ajithittan <ajithittan@gmail.com>
2023-07-07 17:46:58 -0400

    ok
    ok

--- pages/Art/index.js
+++ pages/Art/index.js
@@ -5,19 +5,22 @@ import SpeedDialComp from './SpeedDialComp'
 import { useEffect,useState } from "react"
 import {getAllImages} from '../../modules/api/UserImages'
 import PaintingSlideShow from './PaintingSlideShow'
+import axios from "axios";
+const _ = require("lodash"); 
 
 const URL_WEB_SOCKET = 'ws://localhost:5551/';
 
-const index = () =>{
+const index = ({images}) =>{
     const listOfVariants = ["MasonryView","MosaicView","MosaicQuiltted","MosaicWoven"]
-    const [imageUrls,setImageUrls] = useState(null)
+    const [imageUrls,setImageUrls] = useState(images)
     const [colsForImages,setColsForImages] = useState(5)
     const [width,setWidth] = useState("30vw")
     const [height,setHeight] = useState("30vw")
     const [variant,setVariant] = useState(0)
-    const [waiting,setWaiting] = useState(true)
+    const [waiting,setWaiting] = useState(true) 
     const [slideShow,setSlideShow] = useState(false)
     const [selectedImage,setSelectedImage] = useState(null)
+    const [chgDispTime,setChgDispTime] = useState(25000)
 
     const [ws, setWs] = useState(null);
     const [trades, setTrades] = useState([]);
@@ -36,12 +39,29 @@ const index = () =>{
       };
     }, []);
     **/
-    
+
+    /**
     useEffect(() =>{
         retrieveImages()
     },[])
+     */
 
-    const retrieveImages = () => getAllImages().then(resp => {setImageUrls(resp);setWaiting(false)})
+     useEffect(() =>{
+      setWaiting(false)
+     },[])
+
+     useEffect(() => {
+      const interval = setInterval(() => {
+        //let first = imageUrls.shift()
+        //imageUrls.push(first); // add to end
+        //setImageUrls([...imageUrls])
+        let shuffled_array = _.shuffle(imageUrls);
+        setImageUrls([...shuffled_array])
+      }, chgDispTime);
+      return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+    }, [])
+
+    //const retrieveImages = () => getAllImages().then(resp => {setImageUrls(resp);setWaiting(false)})
     const addColumns = () => setColsForImages(colsForImages + 1)
     const redColumns = () => setColsForImages(colsForImages - 1)
     const changeVariant = () => variant === listOfVariants.length - 1 ? setVariant(0) : setVariant(variant + 1)
@@ -67,6 +87,21 @@ const index = () =>{
             <SpeedDialComp plusOne={addColumns} reduceOne={redColumns} changeMode={changeVariant}></SpeedDialComp>
         </div>
     )
+}
+
+export async function getServerSideProps({ req, res }) {
+  const returnval = await axios.get(process.env.IMAGES_URL, {
+    withCredentials: true,
+    headers: {
+        Cookie: req.headers.cookie
+    }
+  });
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=360, stale-while-revalidate=59'
+  )
+  const data = await returnval.data;
+  return {props: { images:data }}
 }
 
 export default index
