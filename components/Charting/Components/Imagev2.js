55678266ebd58b3277f0f5f72c9d09885e07d962
ajithittan <ajithittan@gmail.com>
2025-07-08 22:18:13 -0400

    ok
    ok

--- components/Charting/Components/Imagev2.js
+++ components/Charting/Components/Imagev2.js
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import * as d3 from "d3";
 
 const Imagev2 = (g,xScale,yScale,xval,yval,imageurl,onclickshow) =>{
     g.append("svg:image")
@@ -9,6 +9,32 @@ const Imagev2 = (g,xScale,yScale,xval,yval,imageurl,onclickshow) =>{
     .attr("y", yScale(yval))
     .style("cursor", "pointer")
     .on("click",() => console.log(onclickshow))
+    .style("cursor", "pointer")
+    .on("mouseover", function(event, d) {
+        // Select the parent group (g) or the SVG element
+        //const g = d3.select(this.parentNode);
+        const [x, y] = d3.pointer(event, this);
+        // Append a text element
+        const text = g.append("text")
+            .attr("x",x) // Adjust position as needed
+            .attr("y", y-10) // Adjust position as needed
+            .attr("dy", ".35em") // Adjust vertical alignment
+            .text(onclickshow) // Display the data value
+            .style("opacity", 0); // Initially hidden
+    
+        // Fade in the text on mouseover
+        text.transition()
+            .duration(200)
+            .style("opacity", 1);
+    })
+    .on("mouseout", function(event, d) {
+        // Fade out the text on mouseout
+        d3.select(this.parentNode).select("text")
+            .transition()
+            .duration(700)
+            .style("opacity", 0)
+            .remove(); // Remove the text element
+    })
 }
 
 export default Imagev2
